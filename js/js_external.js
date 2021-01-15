07489e289d201a2261284e50193628ea187fdf6a
Chronis Fiflis <76593968+chronisf@users.noreply.github.com>
2021-01-15 16:01:39 +0200

    Add files via upload

---
+++ js/js_external.js
@@ -0,0 +1,14 @@
+
+    //text
+document.write("My First Page");    
+    //Html Tags
+document.write("<h1> Hello JS </h1> <p>text</p> <br>");
+    
+ //var value    
+var y="The World of JavaScript"; 
+document.write("<br>");      
+document.write(y);  
+
+    //text + var value
+var x=45;
+document.write("Η τιμή της μεταβλητής x είναι ίση με "+x);          
